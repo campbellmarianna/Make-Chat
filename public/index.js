51887c52d306b6b7706621e0c808aa26259db132
Marianna <mckd2018@gmail.com>
2019-04-24 14:46:35 -0700

    [feat] added to socket connection using vanilla javascript"

--- public/index.js
+++ public/index.js
@@ -1,4 +1,6 @@
-$(document).ready( () => {
-    // Connect to the socket.io server
+// public/index.js
+// When the DOM is loaded create the socket connection
+function ready() {
     const socket = io.connect();
-})
+}
+document.addEventListener("DOMContentLoaded", ready);
