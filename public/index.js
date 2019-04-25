8f69aa39dbdcf9bb239dc01d7917b98d5bb2d531
Marianna <mckd2018@gmail.com>
2019-04-25 16:57:15 -0700

    [feat] Users can log in

--- public/index.js
+++ public/index.js
@@ -1,6 +1,20 @@
 // public/index.js
 // When the DOM is loaded create the socket connection
-function ready() {
+$(document).ready( () => {
+    // Connect to the io(server)
     const socket = io.connect();
-}
-document.addEventListener("DOMContentLoaded", ready);
+
+    $('#createUserBtn').click((e)=>{
+        e.preventDefault();
+        let username = $('#usernameInput').val();
+        if(username.length > 0){
+            // Emit to the server the new user
+            socket.emit('new user', username);
+            $('.usernameForm').remove();
+        }
+    });
+    // socket listeners
+    socket.on('new user', (username) => {
+        console.log(`✋  ${username} has joined the chat! ✋ `);
+    })
+})
